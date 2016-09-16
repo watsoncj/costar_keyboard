73fa17b4383f19e0c0273143cf1154940c95bc3b
Casey Watson <watsoncj@gmail.com>
2016-09-16 10:10:18 -0600

    smarter flash script

--- keyboard/flash.sh
+++ keyboard/flash.sh
@@ -1,5 +1,10 @@
-#!/usr/bin/env bash -xe
-echo "Use both shift keys to enter bootloader"
+#!/usr/bin/env bash -e
+if [[ $(system_profiler SPUSBDataType) != *ATmega32U2*  ]]; then
+  echo -n "Waiting for bootloader..."
+  sleep 5
+fi
+
+# sudo is not needed for these commands
 dfu-programmer atmega32u2 erase
 dfu-programmer atmega32u2 flash ../binaries/flake_20140521_WATSONCJ.hex
 dfu-programmer atmega32u2 start
